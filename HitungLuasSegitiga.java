6ec70964e40c2618fc069a436b0a7327a6ea320c
luhkartikasari <127809285+luhkartikasari@users.noreply.github.com>
2024-04-05 12:01:35 +0800

    Add files via upload

---
+++ HitungLuasSegitiga.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Kartika
+ */
+public class HitungLuasSegitiga {
+    public static void main(String[] args) {
+        // Nilai alas
+        double alas = 30; // dalam satuan cm
+        
+        // Nilai tinggi
+        double tinggi = 60; // dalam satuan cm
+        
+        // Menghitung luas segitiga
+        double luas = (alas * tinggi) / 2;
+        
+        // Menampilkan hasil
+        System.out.println("Luas segitiga dengan alas 30 cm dan tinggi 60 cm adalah: " + luas + " cm^2");
+    }
+}
